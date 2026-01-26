d29d3dafb399c31693abe2b8a4a28379ce1bafca
Mathieu <mathieu.mt.nguyen@gmail.com>
2026-01-26 20:58:06 +0100

    commit test

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,20 +2,35 @@
  * @author : Mathieu
  */
 
-import java.util.ArrayList;
-
 public class Player{
 
     private final String name;
     private int score;
     private static int count = 0;
-    private static ArrayList<Player> allPlayers = new ArrayList<>();
+    private static final int MAX_PLAYERS = 100;
+    private static Player[] allPlayers = new Player[MAX_PLAYERS];
+
+    /**.
+     * @param name Name of player
+     */
 
     public Player(String name){         //Player struct
         this.name = name;
         this.score = 0;
-        count++;
-        allPlayers.add(this);
+        if (count < MAX_PLAYERS) {
+            allPlayers[count] = this;
+            System.out.println("New player " + this.toString());
+            count++;
+        } else {
+            System.err.println("Error : Max capacity");
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getScore(){
+        return score;
     }
 
     public void addScore(int scores){          //Add score to the player
@@ -64,9 +79,8 @@ public class Player{
 
     public static void showPlayers(){
         System.out.println("\n--- Player List ---");
-        for (int i = 0; i < allPlayers.size(); i++){
-            Player p = allPlayers.get(i);
-            System.out.println("~ "+ p.toString());
+        for (int i = 0; i < count; i++){
+            System.out.println("~ "+ allPlayers[i].toString());
         }
     }
 
@@ -74,30 +88,30 @@ public class Player{
         Player.getCount();
 
         Player alice = new Player("Alice");            //Create a player with name : Alice
-        Player Bob = new Player("Bob");
+        Player bob = new Player("Bob");
 
         System.out.println("Game is starting...\n");
 
         System.out.println(alice);
-        System.out.println(Bob);
+        System.out.println(bob);
 
         System.out.println("\nModification\n");
 
         alice.addScore(1);
-        Bob.addScore(2);
+        bob.addScore(2);
 
         System.out.println(alice);
-        System.out.println(Bob);
+        System.out.println(bob);
 
         //Level 5 Part
         Player bobUpper = new Player("BOB");
-        Player b = Bob;
+        Player b = bob;
 
         System.out.println("Alice equals 'Alice' : " + alice.equals("Alice"));
-        System.out.println("Alice equals Bob : " + alice.equals(Bob));
-        System.out.println("Bob equals BOB : " + Bob.equals(bobUpper));
-        System.out.println("Bob == BOB : " + (Bob == bobUpper));
-        System.out.println("Bob == b : " + (Bob == b));
+        System.out.println("Alice equals Bob : " + alice.equals(bob));
+        System.out.println("Bob equals BOB : " + bob.equals(bobUpper));
+        System.out.println("Bob == BOB : " + (bob == bobUpper));
+        System.out.println("Bob == b : " + (bob == b));
 
         Player p3 = new Player();
         Player p4 = new Player();
