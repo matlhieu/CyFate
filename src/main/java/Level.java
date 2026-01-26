ecf72aa9997ece3df4c87cf3c45d4a1c23a6f327
Mathieu <mathieu.mt.nguyen@gmail.com>
2026-01-26 15:48:55 +0100

    commit test

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -3,28 +3,28 @@
  */
 
 public class Level {
-    private char[][] wall;
-    private int length;
-    private int width;
+    private final char[][] maze;
+    private final int length;
+    private final int width;
     private static int numberlevel = 1;
 
     public Level(int length, int width){
         this.length = length;
         this.width = width;
-        wall = new char[length][width];
+        maze = new char[length][width];
         for(int i = 0; i < length; i++){
             for(int j = 0 ; j < width;j++){
-                if(i == 0 || j == 0 || i == length-1 || j == width-1){ // add wall only on the border
-                    wall[i][j] = '#';
+                if(i == 0 || j == 0 || i == length-1 || j == width-1){ // add maze only on the border
+                    maze[i][j] = '#';
                 }
-                else wall[i][j] = ' ';                                 //Empty the interior
+                else maze[i][j] = ' ';                                 //Empty the interior
             }
         }
     }
 
     public void addObstacle(int x, int y){
         if(x > 0 && x < length-1 && y > 0 && y < width-1){
-            wall[x][y] = '#';
+            maze[x][y] = '#';
         }
     }
 
@@ -32,7 +32,7 @@ public class Level {
         System.out.println("\n------------ Level " + numberlevel + "------------");
         for(int i = 0; i < length; i++) {
             for (int j = 0; j < width; j++) {
-                System.out.print(wall[i][j]+ " ");
+                System.out.print(maze[i][j]+ " ");
             }
             System.out.println();
         }
@@ -42,21 +42,21 @@ public class Level {
     public void placePlayer(Player player){
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < width; j++) {
-                if(wall[i][j] == '#'){
+                if(maze[i][j] == '#'){
 
                 }
-                if ( wall[i][j] != '#' && (i == 0 || j == 0 || i == length-1 || j == width-1)){
-                    wall[i][j] = '1';
+                if ( maze[i][j] != '#' && (i == 0 || j == 0 || i == length-1 || j == width-1)){
+                    maze[i][j] = '1';
 
                 }
             }
         }
     }
     public static void main(String[] args){
-        Level level1 = new Level(12, 12);
+        Level level1 = new Level(10, 12);
         level1.generateLevel();
 
-        Level level2 = new Level(5, 12);
+        Level level2 = new Level(5, 18);
         level2.addObstacle(2,5);
         level2.generateLevel();
 
