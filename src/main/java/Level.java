44a8dfd545b3b65a955d9e9d9555823e0e2ee60c
Mathieu <mathieu.mt.nguyen@gmail.com>
2026-01-26 14:01:17 +0100

    commit test

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -2,7 +2,37 @@
  * @author : Mathieu
  */
 
-import java.util.ArrayList;
-
 public class Level {
+    private static char[][] wall;
+    private static final int length = 20;
+    private static final int width = 20;
+
+    public Level(String level){
+        wall = new char[length][width];
+        for(int i = 0; i < length; i++){
+            for(int j = 0 ; j < width;j++){
+                wall[i][j] = level.charAt(i);
+
+            }
+        }
+    }
+
+    public void generateLevel(char[][] level) {
+        for(int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                level[i][j] = wall[i][j];
+                System.out.print(level[i][j] + " ");
+            }
+        }
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("Level 1");
+        Level level1 = new Level("level 1");
+        level1.generateLevel(wall);
+
+
+    }
+
 }
